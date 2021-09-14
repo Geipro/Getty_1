86dd9b749f4ff7abab290b0808997f6fb2dbc923
맹창영 <maengchang@gmail.com>
2021-09-14 10:23:55 +0900

    자격득실확인서 생성

--- creteDatesets/자격득실확인/main.py
+++ creteDatesets/자격득실확인/main.py
@@ -1,5 +1,9 @@
-FamilyName = ['김', '이', '박', '최', '신', '석', '목', '맹', '정']
 from PIL import Image, ImageFont, ImageDraw
+from itertools import permutations as pm
+
+Name_First = ['김', '이', '박', '최', '신', '석', '목', '맹', '정', '장']
+Name_Second = ['', '창', '정', '상', '명', '지', '범', '우', '유', '익', '지']
+Name_Last = ['숙', '영', '준', '원', '지', '훈', '수', '진', '림', '선', '우']
 
 img = Image.open("자격득실확인서.png")
 
@@ -26,4 +30,4 @@ draw = ImageDraw.Draw(img)
 img.save("자격after.png")
 new_img = Image.open("자격after.png")
 new_img.show()
-print(img.size, new_img.size)
+print(len(Name_First), len(Name_Second))

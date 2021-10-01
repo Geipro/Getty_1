af4ca4be698c2fda61eb42f62caba0c9ac628027
meantint <mistinca@naver.com>
2021-10-01 17:15:32 +0900

    update create_loan

--- backend/fastapi/app/database/crud.py
+++ backend/fastapi/app/database/crud.py
@@ -8,19 +8,21 @@ import bcrypt
 def get_user(db: Session, user_id: int):
     return db.query(models.Client).filter(models.Client.cid == user_id).first()
 
+
 def get_user_by_userid(db: Session, user_id: str):
     return db.query(models.Client).filter(models.Client.user_id == user_id).first()
+
 
 def create_user(db: Session, user: schemas.UserCreate):
     # fake_hashed_password = user.user_pw + "notreallyhashed"
     hashed_password = bcrypt.hashpw(user.user_pw.encode("utf-8"), bcrypt.gensalt())
-    print(1234,"   ", hashed_password)
+    print(1234, "   ", hashed_password)
     # 현재 날짜 가져오기
     create_at = date.today()
 
     db_user = models.Client(
-        user_id=user.user_id, 
-        user_pw=hashed_password, 
+        user_id=user.user_id,
+        user_pw=hashed_password,
         user_name=user.user_name,
         phone_number=user.phone_number,
         create_date=create_at,
@@ -28,7 +30,7 @@ def create_user(db: Session, user: schemas.UserCreate):
         birth=user.birth,
         sex=user.sex,
         salary=user.salary,
-        address=""
+        address="",
     )
     db.add(db_user)
     db.commit()
@@ -37,12 +39,21 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 
 def get_loan(db: Session):
-    return db.query(models.UserLoan).all()
+    # print(f"db.query(models.LoanProduct).all() = {db.query(models.LoanProduct).all()}")
+    return db.query(models.LoanProduct).all()
 
 
 def create_loan(db: Session, loan: schemas.LoanCreate):
-    db_loan = models.UserLoan(cid=loan.cid, lid=loan.lid)
-    db.add()
+    db_loan = models.LoanProduct(
+        loan_name=loan.loan_name,
+        loan_age=loan.loan_age,
+        loan_salary=loan.loan_salary,
+        loan_address=loan.loan_address,
+        loan_job=loan.loan_job,
+        interest_rate=loan.interest_rate,
+        loan_amount=loan.loan_amount,
+    )
+    db.add(db_loan)
     db.commit()
     db.refresh(db_loan)
     return db_loan

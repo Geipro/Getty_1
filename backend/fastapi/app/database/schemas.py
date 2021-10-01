5d997d77a2558e5b4f6d4d62ae3d837958cf42cd
meantint <mistinca@naver.com>
2021-10-01 18:19:00 +0900

    create LoanID, ClientID, BankerID

--- backend/fastapi/app/database/schemas.py
+++ backend/fastapi/app/database/schemas.py
@@ -3,36 +3,40 @@ from typing import List, Optional
 from enum import Enum
 from pydantic import BaseModel
 
+
 class User(BaseModel):
-    user_id: str 
-    user_name: str 
-    phone_number: str 
+    user_id: str
+    user_name: str
+    phone_number: str
     create_date: date
     address: str
     job: str
     birth: str
     sex: int
     salary: int
+
     class Config:
         orm_mode = True
 
+
 # 회원가입
-class UserCreate(BaseModel): 
-    user_id: str 
+class UserCreate(BaseModel):
+    user_id: str
     user_pw: str
-    user_name: str 
-    phone_number: str 
+    user_name: str
+    phone_number: str
     job: str
     birth: str
     sex: int
     salary: int
 
+
 # 로그인
-class UserLogin(BaseModel): 
-    user_id: str 
+class UserLogin(BaseModel):
+    user_id: str
     user_pw: str
-    
-    
+
+
 class Token(BaseModel):
     Authorization: str
 
@@ -40,9 +44,9 @@ class Token(BaseModel):
 class UserToken(BaseModel):
     cid: int
     user_id: str
-    user_pw: str 
-    user_name: str 
-    phone_number: str 
+    user_pw: str
+    user_name: str
+    phone_number: str
     create_date: date
 
     class Config:
@@ -61,3 +65,15 @@ class LoanCreate(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class LoanID(BaseModel):
+    lid: int
+
+
+class ClientID(BaseModel):
+    cid: int
+
+
+class BankerID(BaseModel):
+    bid: int

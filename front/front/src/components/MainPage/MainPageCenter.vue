97337aa6ca785f5aec6457f70592f19eb65adc14
석정준 <sjj-92@hanmail.net>
2021-10-01 16:02:53 +0900

    update: design logo, color

--- front/front/src/components/MainPage/MainPageCenter.vue
+++ front/front/src/components/MainPage/MainPageCenter.vue
@@ -1,6 +1,7 @@
 <template>
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner" style="height: 600px">
+    <!-- <div class="carousel-inner" style="height: 600px"> -->
+    <div class="carousel-inner mb-5 mt-3" style="height: mx-auto">
         <div class="carousel-item active">
             <img src="@/assets/loan1.png" class="d-block w-100" alt="...">
         </div>

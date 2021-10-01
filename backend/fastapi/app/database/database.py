e9edefc70455c5097c2708c9ba87999c1689a7ca
meantint <mistinca@naver.com>
2021-10-01 13:25:18 +0900

    update comment

--- backend/fastapi/app/database/database.py
+++ backend/fastapi/app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
+# Project DB Connect
 engine = create_engine(
     "mysql+pymysql://j5a205:1234@j5a205.p.ssafy.io:3306/Project2", convert_unicode=False
 )
@@ -10,24 +10,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# Base.metadata.create_all(engine)
-# Input Code
-# put = """
-# INSERT INTO items(id, title, description, owner_id)
-# VALUES(NULL, 'Wa@!', 'WA@#!', NULL)
-# """
-# print(f"put = {put}")
-# gg = engine.execute(put)
-# print(f"gg = {gg}")
-
-# Read Code
-# df = pd.read_sql("SELECT * FROM items", engine)
-# print(df)
-
-# put = """
-# INSERT INTO items(title, description, owner_id)
-# VALUES('Wa!', 'WA#!', 3)
-# """
-# df = pd.DataFrame.to_sql("items", put, con=engine)
-# print(df)

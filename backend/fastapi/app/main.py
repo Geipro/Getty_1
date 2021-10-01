66fa4441f438de633e1597c1c4fb827e2829b2f1
meantint <mistinca@naver.com>
2021-10-01 17:15:07 +0900

    update read_loan

--- backend/fastapi/app/main.py
+++ backend/fastapi/app/main.py
@@ -10,6 +10,7 @@ from .database import crud, schemas, models
 from .common.consts import JWT_SECRET, JWT_ALGORITHM
 from .database.database import SessionLocal, engine
 
+# import json
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -24,25 +25,8 @@ def get_db():
     finally:
         db.close()
 
-# def create_app():
-#     """
-#     앱 함수 실행
-#     :return:
-#     """
-#     app = FastAPI()
-    
-#     # 라우터 정의
-#     app.include_router(auth.router, tags=["Authentication"], prefix="/auth")
-#     return app
 
-
-# app = create_app()
-
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", reload=conf().PROJ_RELOAD)
-
-@app.post("/signup", status_code=200, response_model= schemas.User)
+@app.post("/signup", status_code=200, response_model=schemas.User)
 async def signup(user_info: schemas.UserCreate, db: Session = Depends(get_db)):
     """
     `회원가입 API`\n
@@ -55,6 +39,7 @@ async def signup(user_info: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="user_id already registered")
     return crud.create_user(db=db, user=user_info)
 
+
 @app.post("/signin", status_code=200, response_model=schemas.Token)
 async def signup(user_info: schemas.UserLogin, db: Session = Depends(get_db)):
     """
@@ -64,17 +49,24 @@ async def signup(user_info: schemas.UserLogin, db: Session = Depends(get_db)):
     :return:
     """
     if not user_info.user_id or not user_info.user_pw:
-        raise HTTPException(status_code=400, detail="user_id and user_pw must be provided")
+        raise HTTPException(
+            status_code=400, detail="user_id and user_pw must be provided"
+        )
     is_exist = crud.get_user_by_userid(db, user_id=user_info.user_id)
     if not is_exist:
         raise HTTPException(status_code=400, detail="NO_MATCH_USER")
-    is_verified = bcrypt.checkpw(user_info.user_pw.encode('utf-8'),is_exist.user_pw.encode('utf-8'))
+    is_verified = bcrypt.checkpw(
+        user_info.user_pw.encode("utf-8"), is_exist.user_pw.encode("utf-8")
+    )
     if not is_verified:
         raise HTTPException(status_code=400, detail="NO_MATCH_USER: Wrong Password")
 
-    print(schemas.UserToken.from_orm(is_exist).dict(exclude={'user_pw','create_date'}))
-    token = dict(Authorization=f"Bearer {create_access_token(data=schemas.UserToken.from_orm(is_exist).dict(exclude={'user_pw','create_date'}),)}")
+    print(schemas.UserToken.from_orm(is_exist).dict(exclude={"user_pw", "create_date"}))
+    token = dict(
+        Authorization=f"Bearer {create_access_token(data=schemas.UserToken.from_orm(is_exist).dict(exclude={'user_pw','create_date'}),)}"
+    )
     return token
+
 
 def create_access_token(*, data: dict = None, expires_delta: int = None):
     to_encode = data.copy()
@@ -83,20 +75,24 @@ def create_access_token(*, data: dict = None, expires_delta: int = None):
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
     return encoded_jwt
 
-@app.get("/user_loan", status_code=200)
-async def read_loan_list(db: Session = Depends(get_db)):
+
+@app.get("/read_loan", status_code=200)
+async def read_loan(db: Session = Depends(get_db)):
     """
     `대출 상품 리스트 가져오기`
     :param db:
     :return:
     """
     db_loan_list = crud.get_loan(db)
-    if db_loan_list:
-        raise HTTPException(status=400, detail="loan error")
+    # print(db_loan_list)
+    if not db_loan_list:
+        raise HTTPException(status_code=400, detail="loan error")
     return db_loan_list
+    # return {}
 
-@app.post("/user_loan", status_code=200, response_model=schemas.LoanCreate)
-async def create_loan(req_info:schemas.LoanCreate, db:Session=Depends(get_db)):
+
+@app.post("/create_loan", status_code=200, response_model=schemas.LoanCreate)
+async def create_loan(req_info: schemas.LoanCreate, db: Session = Depends(get_db)):
     """
     `대출 상품 리스트 추가`
     :param req_info:

0f6e2dd620f7a72c2309d4dbcd2dabcab1f43d17
석정준 <sjj-92@hanmail.net>
2021-10-05 14:33:31 +0900

    add: banker page button & head line

--- front/front/src/components/MainPage/Navbar.vue
+++ front/front/src/components/MainPage/Navbar.vue
@@ -5,7 +5,7 @@
       <router-link :to="{ name: 'Home' }">
         <img src="@/assets/logo.png" style="width:70px">
       </router-link>
-      <!-- 싸피은행 -->
+      싸피은행
     </div>
     <!-- 없어도 될 듯 -->
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">

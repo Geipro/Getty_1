fe0625fa980a02c4bae5d57c9597d18aacfb565e
석정준 <sjj-92@hanmail.net>
2021-10-07 14:58:46 +0900

    test mainpage

--- front/front/src/views/Home.vue
+++ front/front/src/views/Home.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="home video-background">
+  <div class="home">
     <Navbar />
     <MainPageCenter />
-    <!-- <div class="video-foreground"> -->
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/pQN9rTUBIG8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <!-- </div> -->
     <MainPageBottom />
   </div>
 </template>

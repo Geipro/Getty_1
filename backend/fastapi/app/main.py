c422820018cfcbda4d08ade50630d9e37d91e404
jmj961103 <jmj961103@naver.com>
2021-10-08 04:03:08 +0900

    update user file data API

--- backend/fastapi/app/main.py
+++ backend/fastapi/app/main.py
@@ -458,6 +458,6 @@ async def get_user_loan_detail(cid: int, lid: int, db: Session = Depends(get_db)
     return {"user_detail": user_detail, "user_files": user_files}
 
 
-@app.get("/data/img_url", status_code=200)
-async def get_user_file_data(file_url: schemas.UserFileURL):
-    return crud.get_user_data(file_url=file_url.file_url)
+@app.get("/data/file_url", status_code=200)
+async def get_user_file_data(file_url: str = Header(None)):
+    return crud.get_user_data(file_url=file_url)

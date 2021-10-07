f2757b6a3d72085b2f8b0944470439f100f31c71
석정준 <sjj-92@hanmail.net>
2021-10-07 18:22:33 +0900

    update: footer

--- front/front/src/components/MainPage/MainPageBottom.vue
+++ front/front/src/components/MainPage/MainPageBottom.vue
@@ -29,30 +29,6 @@
         <h4 class="col" >신차 구입 예정 고객에게 차량구입 대금을 지원해 드립니다.</h4>
       </div>
     </div>
-
-
-  <!-- <div class="row" v-if="isLogin">
-    <div class="col ml-5">
-      <h3 class="mb-4 text-center">싸피 은행의 대출 추천 상품을 소개합니다.</h3>
-      <ul class="align-items-center text-center list-group list-group-horizontal offset-4">
-        <li class="list-group-item">
-          <h5 style="color: blue">싹편한 언택트 대출</h5><br>
-          365일 언제 어디서나<br>
-          내가 필요한 시점에 신청
-        </li>
-        <li class="list-group-item">
-          <h5 style="color: blue">싹편한 전세 대출</h5><br>
-          온라인으로 한도 확인하고<br>
-          실제 대출 여부 선택 가능
-        </li>
-        <li class="list-group-item">
-          <h5 style="color: blue">싸피주택대출</h5><br>
-          최대 30년까지 분할상환<br>
-          가능한 담보대출
-        </li>
-      </ul>
-    </div>
-	</div> -->
 </div>
 </template>
 

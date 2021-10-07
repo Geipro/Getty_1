b4a62c068b412eabd2435afb0454d6c24db75e4d
meantint <mistinca@naver.com>
2021-10-07 23:37:22 +0900

    Fix : ...

--- backend/fastapi/app/main.py
+++ backend/fastapi/app/main.py
@@ -456,6 +456,6 @@ async def get_user_loan_detail(cid: int, lid: int, db: Session = Depends(get_db)
     return {"user_detail": user_detail, "user_files": user_files}
 
 
-@app.get("/data/img_url/{img_url}", status_code=200)
+@app.get("/data/img_url", status_code=200)
 async def get_user_file_data(file_url:str):
     return crud.get_user_data(file_url=file_url)

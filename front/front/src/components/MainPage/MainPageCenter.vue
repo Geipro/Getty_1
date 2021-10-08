5259d729df73a65ad2f0d8e4b6afb9f04717c6c8
Proscream <proscream@naver.com>
2021-10-08 10:59:27 +0900

    update : iframe margin

--- front/front/src/components/MainPage/MainPageCenter.vue
+++ front/front/src/components/MainPage/MainPageCenter.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="mb-3 pb-3">
   <router-link :to="{ name: 'LoanList' }" style="text-decoration: none;">
     <div class="row justify-content-center align-items-center" style="height: 720px; text-align: left; pointer-events: none; background-color: #EBF5FB;">
         <div class="ml-5">
@@ -13,7 +13,7 @@
   </router-link>
 
   <!-- test1 : color: 흰색   -->
-  <div style="height: 720px; text-align: center;">
+  <div class="mb-3 pb-3" style="height: 720px; text-align: center;">
     <h1 class="pt-5" >복잡한 서류 제출을 내가 원하는 시간에 간편하게</h1>
     <h3 class="pb-3 pt-3" style="color: grey">주말이나 공휴일에도 신청할 수 있습니다.</h3>
     <iframe width="900px" height="560px" src="https://www.youtube.com/embed/pQN9rTUBIG8?autoplay=1&mute=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
